7de380f18a5bc9bac7836824dc44213994620450
Alan <aln447@gmail.com>
2021-03-03 21:24:55 +0100

    Declare a send function
    It will have our lambda's result

--- functions/check.js
+++ functions/check.js
@@ -2,4 +2,11 @@ const axios = require('axios');
 
 exports.handler = function(event, context, callback) {
     const { url } = JSON.parse(event.body);
+
+    const send = (message) => {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({message}),
+        })
+    }
 }
